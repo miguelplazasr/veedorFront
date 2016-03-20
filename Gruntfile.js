56ae058d9d3d1a994ce07fb0cc87005672f5db44
miguelplazasr <miguelplazasr@gmail.com>
2016-03-20 00:18:11 -0500

    Update .gitignore

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,20 @@ module.exports = function(grunt){
 	grunt.loadNpmTasks('grunt-bower-task');
 	
 	grunt.initConfig({
+		bower: {
+			install: {
+				options: {
+					targetDir: 'app/lib',
+					layout: 'byComponent',
+					install: true,
+					verbos: true,
+					cleanBowerDir: true
+				}
+			}
+		}
 		
 	});
 };
+
+// loadNpmTasks
+grunt.loadNpmTasks('grunt-bower-task');
